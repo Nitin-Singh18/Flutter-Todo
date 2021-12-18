65518a4f6e3b2b4df70bac8fcfc45d0f6f31cf28
Nitin Singh <i.nitinsingh18@gmail.com>
2021-12-19 01:26:24 +0530

    Started (ToDo)

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import 'Pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "To Do App",
+      home: HomePage(),
+    );
+  }
+}
