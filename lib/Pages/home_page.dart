fef497cf79f878c9d9cc70f045245aa22a361381
Nitin Singh <i.nitinsingh18@gmail.com>
2021-12-20 00:56:46 +0530

    UI and Database

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:todo_app/Models/dphelper.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +11,83 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final dphelper = Databasehelper.instance;
+
+  final TextEditingController intask = TextEditingController();
+
+  String errtext = "";
+  bool validated = true;
+
+  void showAlertDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(builder: (BuildContext context, setState) {
+            return AlertDialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(2),
+              ),
+              title: Text("Add Task"),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: intask,
+                    decoration: InputDecoration(
+                        hintText: "Task",
+                        errorText: validated ? null : errtext),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      ElevatedButton(
+                          onPressed: () {
+                            if (intask.text.isEmpty) {
+                              setState(
+                                () {
+                                  if (intask.text.isEmpty) {
+                                    errtext = "Task field can't be empty";
+                                    validated = false;
+                                  }
+                                },
+                              );
+                            } else if (intask.text.length > 512) {
+                              setState(
+                                () {
+                                  errtext = "More than 512 characters";
+                                  validated = false;
+                                },
+                              );
+                            }
+                          },
+                          child: Text("Add"))
+                    ],
+                  )
+                ],
+              ),
+            );
+          });
+        });
+  }
+
+// TODO: implement widget
+  Widget mycard(String task) {
+    return Card(
+      elevation: 4,
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Container(
+        child: ListTile(
+          title: Text("$task"),
+          onLongPress: () {
+            print("Item deleted");
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +99,26 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         backgroundColor: Colors.orange.shade100,
         elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showAlertDialog();
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
+        backgroundColor: Colors.orange.shade100,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            mycard("I have to fill my reappear form before the last date."),
+            mycard("Build my concept strong in flutter"),
+            mycard("Work on our website"),
+            mycard("Cover my syllabus before the sessional")
+          ],
+        ),
       ),
     );
   }
